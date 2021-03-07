1534f299d71259e40bd00d17f2c44c077c1a4d8c
umutkeltek <umut.keltek@gmail.com>
2021-03-07 03:10:38 +0300

    First commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    Transform transform;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float mainRotate = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust*Time.deltaTime);
+            Debug.Log("Thrust Working");
+        }
+        
+        
+    }
+    void ProcessRotation()
+    {   if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)){}
+        else if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(mainRotate);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-mainRotate);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; //freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime );
+        rb.freezeRotation = false;
+    }
+}
